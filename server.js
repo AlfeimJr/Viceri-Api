659c0c4cf53c51edfbf7b6fc9cdf7185ee442866
Renovato <gabriel.renovato@docato.com.br>
2024-11-29 11:09:07 -0300

    ajustando porta

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = require("./app");
 const PORT = process.env.PORT || 3000;
 const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
 
-app.listen(PORT, () => {
+app.listen({ port: PORT, host: "0.0.0.0" }, () => {
   console.log(`Servidor rodando em ${BASE_URL} ${PORT}`);
 });
