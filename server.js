0f9fe7d9ff7e1c5790f6603fcd3d1c67749521bd
Renovato <gabriel.renovato@docato.com.br>
2024-11-29 01:59:45 -0300

    first commit

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 3000;
 const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em ${BASE_URL}`);
+  console.log(`Servidor rodando em ${BASE_URL} ${PORT}`);
 });
