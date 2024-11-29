0ab8b95b835bbc6cab6be5833a560c58077489bb
Renovato <gabriel.renovato@docato.com.br>
2024-11-28 22:42:07 -0300

    fix:ajustando as pastas

--- routes/form-fields.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getFormFields } = require("../services/form-fields.service");
-
-// Rota para obter os campos do formulário
-router.get("/", getFormFields);
-
-module.exports = router;
